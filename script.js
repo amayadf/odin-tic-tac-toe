6231c1b1eee9c56dd6d86694ce43b8eb43a91fc0
amayadf <106173666+amayadf@users.noreply.github.com>
2024-03-03 15:58:20 -0500

    Add gameBoard object

---
+++ script.js
@@ -0,0 +1,35 @@
+const gameBoard = (function() {
+    let gameBoardArray = 
+    [['','','']
+     ['','','']
+     ['','','']];
+
+    function getGameBoard() {
+        return gameBoardArray;
+    }
+
+    function checkIfAvailable(x, y) {
+        return gameBoardArray[x][y] === '';
+    }
+
+    function placeMark(x, y, mark) {
+        if(checkIfAvailable(x, y)) {
+            gameBoardArray[x][y] = mark;
+        }
+        else {
+            return false;
+        }
+    }
+
+    function clearGameBoard() {
+        gameBoardArray.forEach((row, rowIndex) => {
+            row.forEach((cell,cellIndex) => {
+                gameBoardArray[rowIndex][cellIndex] = '';
+            });
+        });
+    }
+    
+    return {
+
+    }
+});
