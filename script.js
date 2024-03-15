8566bdf79e7477605aad815789e65792a427e84d
amayadf <106173666+amayadf@users.noreply.github.com>
2024-03-15 13:40:20 -0400

    Change array to unidimensional and adjust code

--- script.js
+++ script.js
@@ -1,49 +1,38 @@
 const gameBoard = (function() {
-    let gameBoardArray = 
-    [['','','']
-     ['','','']
-     ['','','']];
+    let gameBoardArray = ['','','','','','','','',''];
 
     function getGameBoard() {
         return gameBoardArray;
     }
 
-    function checkIfAvailable(x, y) {
-        return gameBoardArray[x][y] === '';
+    function checkIfAvailable(index) {
+        return gameBoardArray[index] === '';
     }
 
-    function placeMark(x, y, mark) {
-        if(checkIfAvailable(x, y)) {
-            gameBoardArray[x][y] = mark;
+    function placeMark(index, mark) {
+        if(checkIfAvailable(index)) {
+            gameBoardArray[index] = mark;
             return true;
         }
-        else {
-            return false;
-        }
+        return false;
     }
 
     function clearGameBoard() {
-        gameBoardArray.forEach((row, rowIndex) => {
-            row.forEach((cell,cellIndex) => {
-                gameBoardArray[rowIndex][cellIndex] = '';
-            });
-        });
+        gameBoardArray.forEach((element,elementIndex) => {
+            gameBoardArray[elementIndex] = '';
+        })
     }
-    
+
     return {
         getGameBoard,
         checkIfAvailable,
         placeMark,
-        clearGameBoard,
+        clearGameBoard
     }
-});
+})
 
-function createPlayer(name, mark) {
+function createPlayer(mark) {
     let points = 0;
-
-    function getName() {
-        return name;
-    }
 
     function getMark() {
         return mark;
@@ -62,7 +51,6 @@ function createPlayer(name, mark) {
     }
 
     return {
-        getName,
         getMark,
         getPoints,
         addPoint,
@@ -70,102 +58,85 @@ function createPlayer(name, mark) {
     }
 }
 
-//I will keep the game rules here since the board object could be ANY board
 const gameController = (function() {
-    let players = [];
-    let currentPlayer;
-    const marks = ['O', 'X'];
+    let players = [createPlayer('O'), createPlayer('X')];
+    let currentPlayerIndex = 0;
 
-    function setPlayers(names, marks) {
-        players.push(createPlayer(names[0], marks[0]));
-        players.push(createPlayer(names[1], marks[1]));
-        currentPlayer = players[0];
+    function checkForTie() {
+        return !gameBoard.getGameBoard().includes('');
     }
 
-    function checkForTie(array) {
-        let tie = true;
-        for(let i = 0; i < array.lenght && tie; i++){
-            for(let j = 0; j < array[i].lenght && tie; j++) {
-                if(row.includes('')) tie = false;   
-            }
-        }
-        return tie;
+    function checkWinner(newMarkIndex) {
+        const winConditions = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
+
+        return winConditions
+                .filter((condition) => condition
+                .includes(newMarkIndex)).some((possibleCondition) => possibleCondition
+                .every((index) => gameBoard.getGameBoard()[index] === players[currentPlayerIndex].getMark()));
     }
 
-    function checkForWin(array) {
-        let winner = false;
-
-        //check horizontal
-        for(let i = 0; i < array.lenght && !winner; i++) {
-            if(allEqual(array[i])) winner = true;
-        }
-
-        //check vertical(
-        for(let i = 0; i < array[0].lenght && !winner; i++) {
-            if(allEqual(array[0][i], array[1][i], array[2][i])) winner = true;
-        }
-
-        //check diagonal
-        if(!winner && allEqual(array[0][0], array[1][1], array[2][2])) winner = true;
-        if(!winner && allEqual(array[2][0], array[1][1], array[0][2])) winner = true;
-
-        return winner;
-    }
-
-    function playRound(x, y) {
-        if(gameBoard.placeMark(x, y, currentPlayer.getMark())) {
-            if(checkForWin(gameBoard.getGameBoard())) {
-                currentPlayer.addPoint();
+    function playRound(index) {
+        if(gameBoard.placeMark(index, players[currentPlayerIndex].getMark())) {
+            if(checkForTie()) {
+                currentPlayerIndex = 0;
                 gameBoard.clearGameBoard();
             }
-            else if (checkForTie(gameBoard.getGameBoard())) {
-                currentPlayer = players[0];
-                gameBoard.clearGameBoard();
+            else if(checkWinner(index).lenght > 0) {
+                players[currentPlayerIndex].addPoint();
+                gameBoard,clearGameBoard();
             }
             else {
                 changePlayer();
             }
         }
     }
-
+    
     function changePlayer() {
-        currentPlayer == players[0] ? currentPlayer = players[1] : currentPlayer = players[1];
+        currentPlayerIndex === 0 ? currentPlayerIndex = 1 : currentPlayerIndex = 0;
     }
 
     function restartGame() {
         gameBoard.clearGameBoard();
         players[0].resetPoints();
         players[1].resetPoints();
-        currentPlayer = players[0];
+        currentPlayerIndex = 0;
     }
 
     return {
-        setPlayers,
+        players,
+        currentPlayerIndex,
         playRound,
-        restartGame,
+        restartGame
     }
 });
 
 const displayController = (function() {
-    let gameScreen = document.querySelector('#game-screen');
-    let gameBoard = document.querySelector('#game-board');
     let gameBoardCells = document.querySelectorAll('.cell');
     let btnRestart = document.querySelector('#btn-restart');
-    let pregameScreen = document.querySelector('#pregame-screen');
-    let btnStart = document.querySelector('#btn-start');
+    let playerOnePoints = document.querySelector('#player-1-points')
+    let playerTwoPoints = document.querySelector('#player-2-points');
 
-    function handleRestartClick() {
+    btnRestart.addEventListener('click', () => {
         gameController.restartGame();
-        //function to make screen look like at the beginning
-    }
+        playerOnePoints.textContent = '0';
+        playerTwoPoints.textContent = '0';
+        gameBoardCells.forEach(cell => cell.textContent = '');
+    });
 
-
-    function handleStartClick() {
-        switchScreens();
-    }
-
-    function switchScreens() {
-        gameScreen.classList.toggle('active');
-        pregameScreen.classList.toggle('active');
-    }
-});
+    gameBoardCells.forEach(cell => cell.addEventListener('click', () => {
+        const index = gameBoardCells.indexOf(cell);
+        gameController.playRound(index);
+        cell.textContent = gameController.players[currentPlayerIndex].getMark();
+        playerOnePoints.textContent = gameController.players[0].getPoints();
+        playerTwoPoints.textContent = gameController.players[1].getPoints();
+    }));
+})
